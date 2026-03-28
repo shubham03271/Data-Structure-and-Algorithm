ae107f64a9c565d9fb3a71265dbc94188bbfece5
shubham03271 <singhshubham13999@gmail.com>
2026-03-28 13:25:07 +0530

    Add files via upload

---
+++ linearSearch.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int>& arr, int x) {
+    
+    // Iterate over the array in order to
+    // find the key x
+    for (int i = 0; i < (int)arr.size(); i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {2, 3, 4, 10, 40};
+    int x = 10;
+    int res = search(arr, x);
+    if (res == -1)
+       cout << "Element is not present in the array";
+    else
+       cout << "Element is present at index " << res;
+    return 0;
+}
